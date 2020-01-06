f098988f5b90267c36896c69d397fb88c5197ecb
Matheus Kurianski <matheus1340@hotmail.com>
2020-01-05 22:23:20 -0300

    Adiciona webhook do Heroku

--- main.py
+++ main.py
@@ -5,13 +5,19 @@ import os
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
-
 def start(update, context):
   update.message.reply_text('Salve!')
 
 def main():
+  TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
+  PORT = int(os.environ.get('PORT', '8443'))
   updater = Updater(token=TELEGRAM_TOKEN, use_context=True)
+
+  updater.start_webhook(listen="0.0.0.0",
+                        port=PORT,
+                        url_path=TELEGRAM_TOKEN)
+  updater.bot.set_webhook("https://padreolival.herokuapp.com/" + TELEGRAM_TOKEN)
+
   dispatcher = updater.dispatcher
 
   dispatcher.add_handler(CommandHandler("start", start))
